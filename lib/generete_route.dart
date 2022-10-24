13e0a629b39cd725822e1747104e55e5e806af09
Iran Junior <iranjuniordev@gmail.com>
2022-10-24 19:12:34 -0300

    feat: adding beta banner when app is conected from conecta

--- lib/generete_route.dart
+++ lib/generete_route.dart
@@ -8,8 +8,8 @@ import 'package:cfit/view/ui/screens/event_city_detail/event_city_detail_route.d
 import 'package:cfit/view/ui/screens/event_public_detail/event_public_detail_arguments.dart';
 import 'package:cfit/view/ui/screens/event_public_detail/event_public_detail_route.dart';
 import 'package:cfit/view/ui/screens/home/home_arguments.dart';
-import 'package:cfit/view/ui/screens/home/home_route.dart';
 import 'package:cfit/view/ui/screens/login/login_route.dart';
+import 'package:cfit/view/ui/screens/map/map_route.dart';
 import 'package:cfit/view/ui/screens/my_datas/my_datas_arguments.dart';
 import 'package:cfit/view/ui/screens/my_datas/my_datas_route.dart';
 import 'package:cfit/view/ui/screens/my_event/my_event_arguments.dart';
@@ -25,42 +25,48 @@ import 'package:flutter/material.dart';
 
 import 'view/ui/screens/generic_error/generic_error_route.dart';
 
+bool fromConecta = false;
+
 Route<dynamic> onGenerateRoutes(RouteSettings settings) {
   final arguments = settings.arguments;
   final name = settings.name?.split('?').first ?? settings.name ?? '/';
   switch (name) {
     case Routes.splash:
-      return SplashRoute().buildRoute();
+      return const SplashRoute().buildRoute(fromConecta: fromConecta);
     case Routes.conecta_events:
       final token = settings.name!.split('?').last.split('=').last;
+      fromConecta = true;
       return SplashRoute(
         token: token,
         initialPage: 2,
-      ).buildRoute();
+      ).buildRoute(fromConecta: true);
     case Routes.conecta_profile:
       final token = settings.name!.split('?').last.split('=').last;
+      fromConecta = true;
       return SplashRoute(
         token: token,
         initialPage: 3,
-      ).buildRoute();
+      ).buildRoute(fromConecta: true);
     case Routes.conecta_dashboard:
       final token = settings.name!.split('?').last.split('=').last;
+      fromConecta = true;
       return SplashRoute(
         token: token,
         initialPage: 0,
-      ).buildRoute();
+      ).buildRoute(fromConecta: true);
     case Routes.conecta_gym:
       final token = settings.name!.split('?').last.split('=').last;
+      fromConecta = true;
       return SplashRoute(
         token: token,
         initialPage: 1,
-      ).buildRoute();
+      ).buildRoute(fromConecta: true);
     case Routes.login:
-      return const LoginRoute().buildRoute();
+      return const LoginRoute().buildRoute(fromConecta: fromConecta);
     case Routes.generic_error:
-      return const GenericErrorRoute().buildRoute();
+      return const GenericErrorRoute().buildRoute(fromConecta: fromConecta);
     case Routes.register:
-      return const RegisterRoute().buildRoute();
+      return const RegisterRoute().buildRoute(fromConecta: fromConecta);
     case Routes.home:
       HomeArguments homeArguments;
       if (arguments != null) {
@@ -69,24 +75,28 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
         homeArguments = HomeArguments();
       }
 
-      return HomeRoute(
-        user: homeArguments.user,
-        initialTab: homeArguments.initialTab,
-      ).buildRoute();
+      // return HomeRoute(
+      //   user: homeArguments.user,
+      //   initialTab: homeArguments.initialTab,
+      // ).buildRoute(fromConecta: fromConecta);
+
+      // TODO: remove this after
+      return const MapRoute().buildRoute(fromConecta: fromConecta);
     case Routes.my_measures:
-      return const MyMeasureRoute().buildRoute();
+      return const MyMeasureRoute().buildRoute(fromConecta: fromConecta);
     case Routes.recover_password:
-      return const RecoverPasswordRoute().buildRoute();
+      return const RecoverPasswordRoute().buildRoute(fromConecta: fromConecta);
     case Routes.complete_account:
       final completeAccountArguments =
           CompleteAccountArguments.fromJson(arguments as String);
       return CompleteAccountRoute(
         conectaUser: completeAccountArguments.conectaUser,
         initialTab: completeAccountArguments.initialTab,
-      ).buildRoute();
+      ).buildRoute(fromConecta: fromConecta);
     case Routes.my_datas:
       final myDatasArguments = MyDataArguments.fromJson(arguments as String);
-      return MyDatasRoute(user: myDatasArguments.user).buildRoute();
+      return MyDatasRoute(user: myDatasArguments.user)
+          .buildRoute(fromConecta: fromConecta);
     case Routes.event_city_details:
       final eventDetailsArguments =
           EventCityDetailsArguments.fromJson(arguments as String);
@@ -94,7 +104,7 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
         eventCity: eventDetailsArguments.eventCity,
         alreadyScheduled: eventDetailsArguments.alreadyConfirmed,
         user: eventDetailsArguments.user,
-      ).buildRoute();
+      ).buildRoute(fromConecta: fromConecta);
     case Routes.event_public_details:
       final eventDetailsArguments =
           EventPublicDetailsArguments.fromJson(arguments as String);
@@ -102,32 +112,41 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
         eventPublic: eventDetailsArguments.eventPublic,
         alreadyScheduled: eventDetailsArguments.alreadyConfirmed,
         user: eventDetailsArguments.user,
-      ).buildRoute();
+      ).buildRoute(fromConecta: fromConecta);
     case Routes.event_public_admin:
       final myEventArguments = MyEventArguments.fromJson(arguments as String);
       return MyEventRoute(
         eventPublic: myEventArguments.eventPublic,
         user: myEventArguments.user,
-      ).buildRoute();
+      ).buildRoute(fromConecta: fromConecta);
     case Routes.event_city_admin:
       final eventCityAdminArguments =
           EventCityAdminArguments.fromJson(arguments as String);
       return EventCityAdminRoute(eventCity: eventCityAdminArguments.eventCity)
-          .buildRoute();
+          .buildRoute(fromConecta: fromConecta);
     case Routes.map:
       final selectLocationArguments =
           SelectLocalizationArguments.fromJson(arguments as String);
       return SelectLocalizationRoute(
         toCreateEvent: selectLocationArguments.toCreateEvent,
         user: selectLocationArguments.user,
-      ).buildRoute<SelectLocalizationResponse?>();
+      ).buildRoute<SelectLocalizationResponse?>(fromConecta: fromConecta);
     default:
   }
-  return const LoginRoute().buildRoute();
+  return const LoginRoute().buildRoute(fromConecta: fromConecta);
 }
 
 extension on Widget {
-  Route<T> buildRoute<T>() {
+  Route<T> buildRoute<T>({bool? fromConecta}) {
+    if (fromConecta == true) {
+      return MaterialPageRoute(
+        builder: (_) => Banner(
+          message: 'BETA',
+          location: BannerLocation.topStart,
+          child: this,
+        ),
+      );
+    }
     return MaterialPageRoute(builder: (_) => this);
   }
 }
